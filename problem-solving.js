353b1d59a48ceaaf8d7bedf0b1b151b4318b64bc
abdullah boshir <96838904+abdullahboshir@users.noreply.github.com>
2025-07-14 20:50:41 +0600

    task 2

--- problem-solving.js
+++ problem-solving.js
@@ -13,3 +13,14 @@ const getMaleNames = (arr) => {
 
 
 
+
+// --------------------Task-2---------------
+const books = [
+  { title: "1984", author: "Orwell", year: 1949 },
+  { title: "The Alchemist", author: "Coelho", year: 1988 },
+  { title: "Atomic Habits", author: "Clear", year: 2018 }
+];
+
+const getBookTitles = (arr) => arr.map(book => book.title);
+
+
